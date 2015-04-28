b36edb007b32d94598f5644d2b986a493c3115e2
Jose Luis Fonseca <desarrollo@reddesignsystems.com>
2015-04-28 14:45:54 -0500

    Analytics

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -74,7 +74,17 @@
         <script src="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.0-rc.2/js/select2.full.min.js"></script>
         <script src="{{asset('/js/all.js')}}"></script>
         <script>
-$('[type="file"]').addClass('form-control');
+        $('[type="file"]').addClass('form-control');
+        </script>
+        <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+          ga('create', 'UA-31272861-11', 'auto');
+          ga('send', 'pageview');
+
         </script>
     </body>
 </html>
