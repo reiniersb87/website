03154c9eaf1950305b7f92b8ff78b6127b1f37cd
Jose Luis Fonseca <desarrollo@reddesignsystems.com>
2015-04-28 15:04:17 -0500

    one more

--- resources/views/sites/index.blade.php
+++ resources/views/sites/index.blade.php
@@ -12,7 +12,7 @@
 @section('content')
 <div class="container">
     <div class="jumbotron">
-        <h1>Hecho en laravel</h1>
+        <h1>Hecho en Laravel</h1>
         <p>Este sitio es una adaptación de <a href="http://builtwithlaravel.com/" target="_blank">Built with Laravel</a> donde se pretende mostrar páginas y Web Apps hechas en Laravel en español.</p>
     </div>
     <hr>
